12a54ba09f602264b39159939d5ca73c670f8540
rickxy <36708000+rickxy@users.noreply.github.com>
2021-12-10 00:24:55 +0300

    update

--- weba/homepage.php
+++ weba/homepage.php
@@ -1,6 +1,26 @@
-
 <?php
-  session_start()
+session_start();
+error_reporting(0);
+include('includes/config.php');
+if(isset($_GET['action']) && $_GET['action']=="add"){
+	$id=intval($_GET['id']);
+	if(isset($_SESSION['cart'][$id])){
+		$_SESSION['cart'][$id]['quantity']++;
+	}else{
+		$sql_p="SELECT * FROM products WHERE id={$id}";
+		$query_p=mysqli_query($con,$sql_p);
+		if(mysqli_num_rows($query_p)!=0){
+			$row_p=mysqli_fetch_array($query_p);
+			$_SESSION['cart'][$row_p['id']]=array("quantity" => 1, "price" => $row_p['productPrice']);
+		
+		}else{
+			$message="Product ID is invalid";
+		}
+	}
+		echo "<script>alert('Product has been added to the cart')</script>";
+		echo "<script type='text/javascript'> document.location ='my-cart.php'; </script>";
+}
+
 ?>
 
 <html>
@@ -119,35 +139,29 @@ body {
   }
 }
 </style>
+
+<!-- <link rel="stylesheet" href="assets/css/main.css"> -->
+<link rel="shortcut icon" href="assets/images/favicon.ico">
+
 </head>
 <body>
 
 <div class="header">
-<img src = "pictures/logo.png" align = "left" ><h1>H2o Website</h1>
+<img src = "pictures/logo.png" align = "left" >
+<h1>H2o Website</h1>
   <p>We are Europe no.1 water suppliers</p>
 </div>
 
 <div class="topnav">
   <a href="product.php">Products</a>
-  <?php
-    if(!isset($_SESSION['user_id'])){
-        echo "<a href='login.php'>Login</a>
-        <a href='Registration.php'>Register</a>";
-    }
-  ?>
-  
+  <a href="login.php">Login</a>
+  <a href="Registration.php">Register</a>
  <a href="ContactUs.php">Contact Us</a>
   <a href="Feedback.php">Feedback</a>
   <a href="">Get a quote</a>
   <a href="registeradmin.php">Register Web Master</a>
   <a href="loginadmin.php">Login Web Master </a>
   <a href="#" style="float:right">chat</a>
-  <?php
-    if(isset($_SESSION['user_id'])){
-        echo "<a href='logout.php'>Logout</a>
-       ";
-    }
-  ?>
  <input type="text" placeholder="Search..">
 </div>
 
@@ -166,8 +180,29 @@ body {
     </div>
     <div class="card">
       <h2>Our famous Products</h2>
-      
-      <div class="image2" style="height:200px;">Image</div>
+      <div class="image2" style="height:350px;">
+      <?php
+$ret=mysqli_query($con,"select * from products  where id between 1 and 5");
+while ($row=mysqli_fetch_array($ret)) 
+{
+	# code...
+
+
+?>					    	
+<div class="det" style="float:left;">
+			
+				<a href="product-details.php?pid=<?php echo htmlentities($row['id']);?>">
+				<img  src="pictures/<?php echo htmlentities($row['productImage1']);?>" data-echo="pictures/<?php echo htmlentities($row['id']);?>/<?php echo htmlentities($row['productImage1']);?>"  width="180" height="300" alt=""></a>
+			
+                <center>
+                <h3 class="name"><a href="product-details.php?pid=<?php echo htmlentities($row['id']);?>"><?php echo htmlentities($row['productName']);?></a></h3>
+                </center>
+           	
+                </div>
+	
+	<?php } ?>
+    </div><!-- /.image -->		
+   
       <p>Famous reviews from our customers</p>
       <p>" I like the service of this company " ... john</p>
     </div>
@@ -175,20 +210,61 @@ body {
   <div class="rightcolumn">
     <div class="card">
       <h2>About Us</h2>
-      <div class="fakeimg" style="height:100px;">Image</div>
+     
+
+      
+      <?php
+$ret=mysqli_query($con,"select * from products limit 1");
+while ($row=mysqli_fetch_array($ret)) 
+{
+	# code...
+?>
+
+
+<div class="fakeimg" style="padding:14px;">
+				<a href="product-details.php?pid=<?php echo htmlentities($row['id']);?>">
+				<img  src="pictures/<?php echo htmlentities($row['productImage1']);?>" data-echo="pictures/<?php echo htmlentities($row['id']);?>/<?php echo htmlentities($row['productImage1']);?>"  width="120" height="120" alt=""></a>
+		
+
+            </div><!-- /.image -->			
+	<?php } ?>
+
       <p>Our address is :   </p>
+      <p>412 - 01500 Calfornia, USA   </p>
+      <p> Call </p>
+      <p>+1-4589-7892   </p>
     </div>
     <div class="card">
       <h3>Popular Recent sold prodcuts</h3>
-      <div class="fakeimg"><p>Image</p></div>
-      <div class="fakeimg"><p>Image</p></div>
-      <div class="fakeimg"><p>Image</p></div>
+
+      <?php
+$ret=mysqli_query($con,"select * from products where id between 10 and 13");
+while ($row=mysqli_fetch_array($ret)) 
+{
+	# code...
+
+
+?>
+
+
+<div class="fakeimg" style="padding:14px;">
+				<a href="product-details.php?pid=<?php echo htmlentities($row['id']);?>">
+				<img  src="pictures/<?php echo htmlentities($row['productImage1']);?>" data-echo="pictures/<?php echo htmlentities($row['id']);?>/<?php echo htmlentities($row['productImage1']);?>"  width="120" height="120" alt=""></a>
+		
+
+            </div><!-- /.image -->			
+	<?php } ?>
+   
     </div>
     <div class="card">
       <h3>Follow Me</h3>
-      <p>logo facebook</p>
-      <p>logo twitter</p>
-       <p>logo instagram</p>
+     	
+      <img  src="assets/images/facebook-logo.png"  width="40" height="40" alt="" style="float:left;"> <p>Facebook</p> <br>
+
+      <img  src="assets/images/twitter-logo.png"  width="40" height="40" alt="" style="float:left"> <p>Twitter</p> <br>
+
+      <img  src="assets/images/instagram-logo.jpg"  width="40" height="40" alt="" style="float:left;"> <p>Instagram</p><br>
+
     </div>
   </div>
 </div>
